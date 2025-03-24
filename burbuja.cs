2b45e6826ca548a43312766b371a082d3d51d7f2
Angel Noh <angelgabrielnohcamargo@gmail.com>
2025-03-24 16:31:05 -0600

    Add files via upload

---
+++ burbuja.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[10] { 8, 3, 7, 1, 9, 4, 2, 10, 6, 5 };
+
+        for (int i = 0; i < numeros.Length - 1; i++)
+        {
+            for (int j = 0; j < numeros.Length - 1 - i; j++)
+            {
+                if (numeros[j] > numeros[j + 1])
+                {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("\nNúmeros ordenados de menor a mayor:");
+        foreach (int num in numeros)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
